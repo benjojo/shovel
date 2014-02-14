fa89cfbd06f0c6671c34aeec19bfadbc942a86bc
Ben Cox <ben@benjojo.co.uk>
2014-02-14 13:35:32 +0000

    MakeNewTable func, fixed logging

--- shovel.go
+++ shovel.go
@@ -8,11 +8,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
-	log.New(os.Stderr, "[Shovel]", log.Ltime)
-	log.Println("Connecting to DB\n")
+	logger := log.New(os.Stderr, "[Shovel]", log.Ltime)
+	logger.Println("Connecting to DB\n")
 	DBHost := flag.String("host", "localhost:3306", "<hostname>:<port>")
 	DBName := flag.String("database", "Shovel", "<dbname>")
 	DBUser := flag.String("user", "root", "<dbuser>")
@@ -22,13 +23,19 @@ func main() {
 	con, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", *DBUser, *DBPass, *DBHost, *DBName))
 	defer con.Close()
 	if err != nil {
-		log.Fatalln("Unable to connect to the database, Aborting.")
+		logger.Fatalln("Unable to connect to the database, Aborting.")
+	}
+	TN := ""
+	if *Tablename == "" {
+		TN = MakeNewTable()
+	} else {
+		TN = *Tablename
 	}
 	bio := bufio.NewReader(os.Stdin)
 	var hasMoreInLine bool = true
 	for hasMoreInLine {
 		line, hasMoreInLine, err := bio.ReadLine()
-
+		InsertIntoDB(string(line), TN)
 	}
 }
 
@@ -36,6 +43,20 @@ func InsertIntoDB(input string, tablename string) {
 
 }
 
-func MakeNewTable(DB *sql.DB) string {
-
+/*
+CREATE TABLE `asdasd` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`line` TEXT NOT NULL,
+	`logtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci';
+*/
+func MakeNewTable(DB *sql.DB, logger *log.Logger) string {
+	NewTableName := fmt.Sprintf("Shovel_%d", int32(time.Now().Unix()))
+	_, e := DB.Exec("CREATE TABLE ? (`id` INT NOT NULL AUTO_INCREMENT,`line` TEXT NOT NULL,`logtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (`id`))COLLATE='utf8_general_ci';", NewTableName)
+	if e != nil {
+		logger.Fatalln("Could not make a table to store the data in.")
+	}
+	return NewTableName
 }
