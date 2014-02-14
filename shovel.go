84ac333a18d24f5df55c50bd555e0c2f46711f55
Ben Cox <ben@benjojo.co.uk>
2014-02-14 13:18:10 +0000

    Added base of the program

--- shovel.go
+++ shovel.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"bufio"
+	// "flag"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+func main() {
+	fmt.Fprint(os.Stderr, "Connecting to DB\n")
+	con, err := sql.Open("mysql", "root:@tcp(localhost:3306)/Shovel")
+	bio := bufio.NewReader(os.Stdin)
+	var hasMoreInLine bool = true
+	for hasMoreInLine {
+		line, hasMoreInLine, err := bio.ReadLine()
+
+	}
+}
+
+func InsertIntoDB(input string) {
+
+}
