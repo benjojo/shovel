4ad8de987bc8d47ebd9f6d87a5746b3c6a7068c6
Ben Cox <ben@benjojo.co.uk>
2014-02-14 13:25:17 +0000

    Added better logging, Added error handling to the database

--- shovel.go
+++ shovel.go
@@ -6,19 +6,23 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Fprint(os.Stderr, "Connecting to DB\n")
+	log.New(os.Stderr, "[Shovel]", log.Ltime)
+	log.Println("Connecting to DB\n")
 	DBHost := flag.String("host", "localhost:3306", "<hostname>:<port>")
 	DBName := flag.String("database", "Shovel", "<dbname>")
 	DBUser := flag.String("user", "root", "<dbuser>")
 	DBPass := flag.String("pass", "", "<dbpass>")
 	Tablename := flag.String("tablename", "", "<tablename> else it will make a new one")
-
+	flag.Parse()
 	con, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", *DBUser, *DBPass, *DBHost, *DBName))
-
+	if err != nil {
+		log.Fatalln("Unable to connect to the database, Aborting.")
+	}
 	bio := bufio.NewReader(os.Stdin)
 	var hasMoreInLine bool = true
 	for hasMoreInLine {
